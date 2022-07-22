1a5a81d561930f4bf23517061495e5602cf67104
Jubair <mjahmed.wd@gmail.com>
2022-07-22 15:59:53 +0600

    init

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    var csInterface = new CSInterface();
+    document.getElementById('btn_test').addEventListener('click', function () {
+        csInterface.evalScript('sayHello()');
+    });
+    
+}());

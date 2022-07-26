2032f1a8eace4644b66589114cb79000dc34b6a6
Jubair <mjahmed.wd@gmail.com>
2022-07-26 18:10:46 +0600

    data passing

--- js/main.js
+++ js/main.js
@@ -2,8 +2,40 @@
     'use strict';
 
     var csInterface = new CSInterface();
-    document.getElementById('btn_test').addEventListener('click', function () {
-        csInterface.evalScript('sayHello()');
-    });
+
     
+    var obj = {
+        str: "Snarky Puppy",
+        num: 42,
+        today: new Date(),
+        nestedObj: {
+            nestedStr: "Mehliana",
+            nestedNum: 8,
+            nestedDate: new Date()
+        }
+    };
+
+    
+    document.getElementById('btn_test').addEventListener('click', function () {
+        csInterface.evalScript('JSXParseObj(' + JSON.stringify(obj) + ')');
+    });
+
+    var extensionRoot = csInterface.getSystemPath(SystemPath.EXTENSION) + "/jsx/";
+
+    // for single file
+    csInterface.evalScript('evalFile("' + extensionRoot + 'anotherFile.jsx")',
+        function (res) {
+            console.log(res)
+        });
+
+
+    // for multiple file
+    csInterface.evalScript('evalFiles("' + extensionRoot + '")', function (res) {
+        console.log(res)
+    });
+
+
+
+
+
 }());
--- jsx/photoshop.jsx
+++ jsx/photoshop.jsx
@@ -1,3 +1,53 @@
-function sayHello() {
-    alert("Say Hello from JSX!");
+function sayHello(str, num) {
+    alert("Photoshop says: " + str + "! Have a great " + num + ".");
 }
+
+function evalFile(path) {
+    try {
+        $.evalFile(path);
+    } catch (e) {
+        alert("Exception:" + e);
+    }
+}
+
+
+function JSXParseObj(o) {
+    var str = "Object received!\n";
+    for (prop in o) {
+        str += prop + " [" + typeof o[prop] + "]: " + o[prop] + ".\n";
+    }
+    alert(str);
+}
+
+function evalFiles(jsxFolderPath) {
+    var folder = new Folder(jsxFolderPath);
+    if (folder.exists) {
+        var jsxFiles = folder.getFiles("*.jsx");
+        for (var i = 0; i < jsxFiles.length; i++) {
+            var jsxFile = jsxFiles[i];
+            try {
+                $.evalFile(jsxFile);
+            } catch (e) {
+                alert(e.message + "\n" + jsxFile);
+            }
+        }
+    }
+}
+
+var actionFile = csInterface.getSystemPath(SystemPath.EXTENSION) + "/action/demo\
+ Action.atn";
+var actionSetName = 'HTML Panels Development Actions'; var runAction = function (actionName, actionSet) {
+    csInterface.evalScript("app.doAction('" + actionName + "', '" + actionSet + "'\ )", function (res) {
+        if (EvalScript_ErrMessage == res) {
+            // something went wrong!
+            csInterface.evalScript("loadActionSet('" + actionFile + "', '" + actionSetName + "')");
+        }
+    })
+}
+
+$('#action1').on('click', function (evt) {
+    runAction('First action', actionSetName);
+});
+$('#action2').on('click', function (evt) {
+    runAction('Second action', actionSetName);
+});
